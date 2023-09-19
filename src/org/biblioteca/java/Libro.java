87ca7ad14ce7b3f8558207be129b39211492d3e0
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-19 14:29:18 +0200

    firstcommit

---
+++ src/org/biblioteca/java/Libro.java
@@ -0,0 +1,20 @@
+package org.biblioteca.java;
+
+public class Libro {
+	
+	private String title;
+	private int pages;
+	private String author;
+	private String publisher;
+	
+	public Libro(String title, int pages, String author, String publisher) {
+//		setTitle(title);
+//		setPages(pages);
+//		setAuthor(author);
+//		setPublisher(publisher);
+	}
+	//GETTER
+	
+	
+	//SETTER
+}
