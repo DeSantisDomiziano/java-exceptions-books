48c84610b626985a5a2ae8098418924dfe715416
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-19 14:41:02 +0200

    add methods getter & setter with Exeptions

--- src/org/biblioteca/java/Libro.java
+++ src/org/biblioteca/java/Libro.java
@@ -14,7 +14,53 @@ public class Libro {
 //		setPublisher(publisher);
 	}
 	//GETTER
+	public String getTitle() {
+		return title;
+	}
 	
+	public int getPages() {
+		return pages;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public String getPublisher() {
+		return publisher;
+	}
 	
 	//SETTER
+	public void setTitle(String title) throws Exception {
+		
+		if (title.isBlank())
+			throw new Exception("title can't be empty");
+		
+		this.title = title;
+	}
+	
+	public void setPages(int pages) throws Exception {
+		
+		if (pages <= 0)
+			throw new Exception("book's pages can't be 0 or less");
+		
+		this.pages = pages;
+	}
+	
+	public void setAuthor(String author) throws Exception {
+		
+		if (author.isBlank())
+			throw new Exception("author can't be empty");
+		
+		this.author = author;
+	}
+	
+	public void setPublisher(String publisher) throws Exception {
+		
+		if (publisher.isBlank())
+			throw new Exception("title can't be empty");
+		
+		this.publisher = publisher;
+	}
+	
 }
